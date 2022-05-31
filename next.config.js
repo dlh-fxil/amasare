ba90a113f6d7d59d910d093993a6abaa0dbc6430
dlh-fxil <89239063+dlh-fxil@users.noreply.github.com>
2022-05-31 15:18:40 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	async rewrites() {
 		return [
 			{
-				source: "backend/:path*",
+				source: "/backend/:path*",
 				destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/:path*",
 			},
 		];
